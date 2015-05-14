dbb71516a758ef93d0f70f55a2192d5dc5c2c165
VGT <tomek@vgtworld.pl>
2015-05-14 20:46:17 +0200

    Create first pane.

--- modular-app/src/main/java/pl/vgtworld/javafx/fxml/modularapp/MainController.java
+++ modular-app/src/main/java/pl/vgtworld/javafx/fxml/modularapp/MainController.java
@@ -2,15 +2,21 @@ package pl.vgtworld.javafx.fxml.modularapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class MainController {
 
 	@FXML
 	private BorderPane root;
 
-	public void firstPressed(ActionEvent actionEvent) {
-		//TODO
+	private FXMLLoader loader = new FXMLLoader();
+
+	public void firstPressed(ActionEvent actionEvent) throws IOException {
+		BorderPane firstPane = loader.load(getClass().getResource("first/first.fxml"));
+		root.setCenter(firstPane);
 	}
 
 	public void secondPressed(ActionEvent actionEvent) {
