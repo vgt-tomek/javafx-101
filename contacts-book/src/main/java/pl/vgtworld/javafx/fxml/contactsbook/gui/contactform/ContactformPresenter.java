aaa46b22120297603bf033b7bb2ce0fd39018478
VGT <tomek@vgtworld.pl>
2015-05-16 21:33:12 +0200

    Fix input fields in contact form.

--- contacts-book/src/main/java/pl/vgtworld/javafx/fxml/contactsbook/gui/contactform/ContactformPresenter.java
+++ contacts-book/src/main/java/pl/vgtworld/javafx/fxml/contactsbook/gui/contactform/ContactformPresenter.java
@@ -2,18 +2,18 @@ package pl.vgtworld.javafx.fxml.contactsbook.gui.contactform;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
 
 public class ContactformPresenter {
 
 	@FXML
-	private Text firstName;
+	private TextField firstName;
 
 	@FXML
-	private Text lastName;
+	private TextField lastName;
 
 	@FXML
-	private Text emailAddress;
+	private TextField emailAddress;
 
 	@FXML
 	private TextArea notes;
