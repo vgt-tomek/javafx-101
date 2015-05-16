faac6aa81b9fd51691704b049c38b41912a61682
VGT <tomek@vgtworld.pl>
2015-05-16 21:31:08 +0200

    Display new contact form after pressing button.

--- contacts-book/src/main/java/pl/vgtworld/javafx/fxml/contactsbook/gui/main/MainPresenter.java
+++ contacts-book/src/main/java/pl/vgtworld/javafx/fxml/contactsbook/gui/main/MainPresenter.java
@@ -3,6 +3,7 @@ package pl.vgtworld.javafx.fxml.contactsbook.gui.main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
+import pl.vgtworld.javafx.fxml.contactsbook.gui.contactform.ContactformView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,5 +16,10 @@ public class MainPresenter implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+	}
+
+	public void displayNewContactForm() {
+		ContactformView newContactForm = new ContactformView();
+		rootPane.setCenter(newContactForm.getView());
 	}
 }
